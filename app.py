8dc84a5760431a3a03bbc29a255e1409a77636d1
CORALINE-PC1\Z370-A <saitarn@coraline.co.th>
2018-08-27 20:03:45 +0700

    init

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
+        # return {'student': name}
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201
+
+    def put(self, name):
+        pass
+    
+    def delete(self, name):
+        pass
+
+api.add_resource(Item, '/item/<string:name>') # http://localhost:5000/student/Rolf
+
+app.run(port=5000, debut=True)
